6fe40400fa09c264df1a8fc639c333bf3fa20cf5
Kate <you-like-me@yandex.ru>
2023-09-27 22:35:34 +0300

    Initial commit

---
+++ js/router.js
@@ -0,0 +1,27 @@
+const route = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    handleLocation();
+  };
+
+const routes = {
+    404: "/pages/404.html",
+    "/": "/pages/home.html",
+    "/about": "/pages/about.html",
+    "/contact": "/pages/contact.html",
+    "/services": "/pages/services.html", // Добавленная страница
+    "/portfolio": "/pages/portfolio.html", // Добавленная страница
+  };
+
+  const handleLocation = async () => {
+    const path = window.location.pathname;
+    const route = routes[path] || routes[404];
+    const html = await fetch(route).then((data) => data.text());
+    document.getElementById("main-page").innerHTML = html;
+  };
+  
+  window.onpopstate = handleLocation;
+  window.route = route;
+  
+  handleLocation();
